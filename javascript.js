e852e3ac1580e9c2257dc6622d6bbe70dab83682
ekrispy <estevan.crispin@gmail.com>
2024-03-22 19:18:27 -0600

    added some new comments to the js file

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 //part 1  
+// 
 
 // const CSVdata = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
@@ -14,6 +15,7 @@
 // }
 
 // part 2
+// was able to put into an array
 
 const CSVdata = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
@@ -27,10 +29,9 @@ for ( let i in rows) {
     let cell = rows[i].split(',')
     firstData.push(cell)
     
-//     console.log(cell)
 }
 console.log("first",firstData)
-// console.log('second',combined)
+
 
 // part 3
 
@@ -48,21 +49,8 @@ console.log("first",firstData)
 // console.log(data)
  
 
-//     for ( let i = 1; i <= data.length; i++ ) {
-//    let object = {}     
-// object.id = firstData[i][0]
-// object.name =firstData[i][1]
-// object.occupation =firstData[i][2]
-// object.age =firstData[i][3]
-// for (let j=0; j<data[0].length; i++) {
-//     console.log(data[0][i])
-// }
-// console.log(object)
-// let data = []
-// data.push(object)
-// console.log(data)
 
-//     }
+// below is a long way to get what we want.. 
 
 // for(let i=1;i<firstData.length;i++){
 //     let object ={}
@@ -81,7 +69,7 @@ console.log("first",firstData)
 //   } 
 
 
-
+// short and easier way to get the right ouput for part 3
 
 let newData = []
 
@@ -89,11 +77,7 @@ let newData = []
 for(let i=1;i<firstData.length;i++){
   let object ={}
 
-  // object.id = firstData[i][0]
-  // object.name = firstData[i][1]
-  // object.occupation = firstData[i][2]
-  // object.age = firstData[i][3]
-  
+ 
 for (let j =0;j<firstData[0].length;j++){
     let key = firstData[0][j].toLowerCase();
     
@@ -107,20 +91,37 @@ newData.push(object)
 
 console.log(newData)
 
-//4
+//part 44
 newData.pop()
 newData.splice(1,0, {id: "48", name: "Barry", occupation: "Runner", age: "25" }) 
 newData.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
 console.log(newData)
 
+// below is a long way to get the average
+// console.log((Number(newData[0].age)+Number(newData[1].age)+Number(newData[2].age)+Number(newData[3].age)+Number(newData[4].age))/5)
 
-console.log((Number(newData[0].age)+Number(newData[1].age)+Number(newData[2].age)+Number(newData[3].age)+Number(newData[4].age))/5)
-
+// short way to get the average
 let total =0
 for(let i=0;i<newData.length;i++){
   total += Number(newData[i].age)
 }
 console.log(total/5)
 
+// part 5
 
-console.log(JSON.stringify(newData))
+
+// below is a long way method to get the same reult as in line 119-124
+// for (let i= 0; i<newData.length;i++){
+// console.log(newData[i].id, ','+ newData[i].name,',' + newData[i].occupation, ',' + newData[i].age)
+
+// }
+// total += Number(newData[i].age)
+// console.log(JSON.stringify(newData))
+
+
+let CSV = "";
+for (let i= 0; i < newData.length; i++) {
+    CSV += newData[i].id+ ',' + newData[i].name+ ',' + newData[i].occupation+ ',' + newData[i].age
+    
+    }
+console.log(CSV)
